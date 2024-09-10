d00df84cd0b86c511415936fd2d98d5b55b1a5ac
Claudio Matheus <claudiomatheus055@gmail.com>
2024-09-10 13:52:56 -0300

    Adicionado as funções matematicas basicas

--- Calculadora/Form1.cs
+++ Calculadora/Form1.cs
@@ -6,5 +6,40 @@ namespace Calculadora
         {
             InitializeComponent();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
